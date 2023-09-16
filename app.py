b41d138924e1454748f6572b683dc0fcdcbcd0f5
chd6ihomis <louiesteven.cadaguman@wvsu.edu.ph>
2023-09-16 16:11:17 +0800

    Version 2

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+username = 'admin'
+
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return 'Hello,' + username + '!'
 
 if __name__ == '__main__':
     app.run()
